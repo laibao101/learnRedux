58c47d64987b9c1dc1f84422fc418d1a728976af
laibao <laibao@laibaodeMacBook-Pro.local>
2017-03-18 21:43:27 +0800

    complete the Explore

--- partSix/src/Components/Explore.js
+++ partSix/src/Components/Explore.js
@@ -19,5 +19,40 @@ export default class Explre extends Component{
 	setInputValue = (val) => {
 		//Generally mutating DOM is a bad idea in React Component
 		//but doing this for a single uncontrolled field is less fus
+		//than making it controlled and maintaining a state for it
+		this.refs.input.value = val;
+	}
+
+	handleKeyUp = e => {
+		if (e.keyCode === 13) {
+			this.handleGoClick();
+		}
+	}
+
+	handleGoClick = () => {
+		this.props.onChange(this.getInputValue());
+	}
+
+	render(){
+		return (
+			<div>
+				<p>Type a username or repo full name and hit 'Go':</p>
+				<input
+					type="text"
+					ref="input"
+					defaultValue={this.props.value}
+					onKeyUp={this.handleKeyUp}
+				/>
+				<button onClick={this.handleGoClick}>
+					Go!
+				</button>
+				<p>
+					Code on <a href={GITHUB_REPO} target="_blank">Github</a>
+				</p>
+				<p>
+					Move the DevTools with ctrl+w or hide them with ctrl+h
+				</p>
+			</div>
+		);
 	}
 }

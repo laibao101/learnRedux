d8625f98df272971ea86a7bad26f61894894c87c
laibao <laibao@laibaodeMacBook-Pro.local>
2017-03-19 21:31:07 +0800

    complete action

--- partSix/src/Actions/index.js
+++ partSix/src/Actions/index.js
@@ -104,6 +104,38 @@ export const STARGAZERS_SUCCESS = 'STARGAZERS_SUCCESS';
 
 export const STARGAZERS_FAILURE = 'STARGAZERS_FAILURE';
 
+//Fetches a page of stargazers for a particular repo.
+//Relies on the custom API middleware defined in ../Middleware/api.js.
+const fetchStargazers = (fullName,nextPageUrl) =>({
+	fullName,
+	[CALL_API]:{
+		types:[STARGAZERS_REQUEST,STARGAZERS_SUCCESS,STARGAZERS_FAILURE],
+		endpoint:nextPageUrl,
+		schema:Schemas.USER_ARRAY,
+	}
+})
+
+export const loadStargazers = (fullName,nextPage) => (dispatch,getState) => {
+	const {
+		nextPageUrl = `repos/${fullName}/stargazers`,
+		pageCount = 0
+	} = getState().pagination.stargazersByRepo[fullName] || {};
+
+	if (pageCount > 0 && !nextPage) {
+		return null;
+	}
+
+	return dispatch(fetchStargazers(fullName,nextPageUrl));
+}
+
+export const RESET_ERROR_MESSAGE = 'RESET_ERROR_MESSAGE';
+
+//Resets the currently visible error message.
+export const resetErrorMessage = () => ({
+	type:RESET_ERROR_MESSAGE
+})
+
+
 //Fetchs a page of stargazers for a particular repo.
 //Bails out if page is cached and user didn't specifically request next page.
 //Relies on Redux Thunk middleware

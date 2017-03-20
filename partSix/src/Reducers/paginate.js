8797b37f69dd44afe35a31c14228c4c3fb2859b5
laibao <laibao@laibaodeMacBook-Pro.local>
2017-03-20 21:23:56 +0800

    paginate

--- partSix/src/Reducers/paginate.js
+++ partSix/src/Reducers/paginate.js
@@ -18,5 +18,57 @@ const paginate = ({ types, mapActionToKey }) => {
 
 	const [requestType, successType, failureType] = types;
 
-	const 
+	const updatePagination = (state => {
+		isFetching:false,
+		nextPageUrl:undefined,
+		pageCount:0,
+		ids:[]
+	},action) => {
+		switch (action.type) {
+			case requestType:
+				return {
+					...state,
+					isFetching:true
+				}
+			case successType:
+				return {
+					...state,
+					isFetching:false,
+					ids:union(state.ids,action.response.result),
+					nextPageUrl:action.response.nextPageUrl,
+					pageCount:state.pageCount + 1
+				}
+			case failureType:
+				return {
+					...state,
+					isFetching:false
+				}
+			default:
+				return state;
+		}
+	}
+
+	return (state = {},action) => {
+		//Update pagination by key
+		switch (action.type) {
+			case requestType:
+			case successType:
+			case failureType:
+				const key = mapActionToKey(action)
+				if (typeof key !== 'string') {
+					throw new Error('expected key to be a string.')
+				}
+				return {
+					...state,
+					[key]:updatePagination(state[key],action)
+				}
+			default:
+				return state;
+		}
+	}
+
+
 }
+
+
+export default paginate;
